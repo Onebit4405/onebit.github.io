1390cf4eee2e16496d0b42952f519673893f2851
Onebit <132233099+Onebit4405@users.noreply.github.com>
2024-03-02 16:36:20 +0900

    Update test.js

--- test.js
+++ test.js
@@ -4,7 +4,8 @@ public class Main {
 	public static void main(String[] args) {
 
 		// Dateクラスを生成 
-		Date date = new Date();}}
+		Date date = new Date();}
+}
 function alertButton() {
     alert(date);
 }
