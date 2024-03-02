93a4147a10fe0c195df39a341c208b71b942d4ae
Onebit <132233099+Onebit4405@users.noreply.github.com>
2024-03-02 16:21:31 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
+import java.util.Date;
+
+public class Main {
+	public static void main(String[] args) {
+
+		// Dateクラスを生成 
+		Date date = new Date();}}
 function alertButton() {
-    alert('ボタンが押されました');
+    alert(date);
 }
